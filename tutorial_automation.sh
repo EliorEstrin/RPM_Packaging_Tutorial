61e2eddaf937e474d6be6ebe74155a2ad84ad936
elior <elior.estrin@develeap.com>
2023-11-14 10:48:49 +0200

    added script

---
+++ tutorial_automation.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+install_packages() {
+  echo "Installing necessary packages..."
+  sudo yum install -y rpmdevtools rpmlint
+}
+
+setup_rpm_build() {
+  echo "Setting up the RPM build environment..."
+  rpmdev-setuptree
+}
+
+prepare_game_package() {
+  echo "Preparing the game package..."
+  mkdir cgame-1.0.0
+  mv cgame.c Makefile cgame-1.0.0/
+}
+
+create_tarball() {
+  echo "Creating a tarball of the game package..."
+  tar -czvf cgame-1.0.0.tar.gz cgame-1.0.0
+}
+
+move_to_rpm_sources() {
+  echo "Moving the tarball to the RPM build SOURCES directory..."
+  mv cgame-1.0.0.tar.gz ~/rpmbuild/SOURCES/
+}
+
+# Main script execution
+echo "Make sure you are inside the repo folder when you run this script"
+install_packages
+setup_rpm_build
+prepare_game_package
+create_tarball
+move_to_rpm_sources
+
+echo "Script execution completed successfully."
